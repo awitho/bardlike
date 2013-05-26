0af83d37cf6a17ff81e421c680d016d07a95c5f2
Alexander Thompson <alexanderwt.95@gmail.com>
2013-05-26 16:57:38 -0400

    Fixed some more formatting.

--- src/MainGameState.java
+++ src/MainGameState.java
@@ -8,13 +8,13 @@ import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class MainGameState extends BasicGameState {
-		private GameMap map;
-		private SpriteSheet playerSprites;
-		private Player player;
-		public TileDictionary tileDictionary;
-		
-		private int transX = 0;
-		private int transY = 0;
+	private GameMap map;
+	private SpriteSheet playerSprites;
+	private Player player;
+	public TileDictionary tileDictionary;
+
+	private int transX = 0;
+	private int transY = 0;
 		
 	public MainGameState() {
 		
@@ -35,51 +35,51 @@ public class MainGameState extends BasicGameState {
 
 	@Override
 	public void update(GameContainer container, StateBasedGame s, int delta) throws SlickException {
-			if (container.getInput().isKeyPressed(Input.KEY_ESCAPE)) {
-				container.exit();
-			}
+		if (container.getInput().isKeyPressed(Input.KEY_ESCAPE)) {
+			container.exit();
+		}
 
-			transX = (player.getX() * -1) + (5*64);
-			transY = (player.getY() * -1) + (5*64);
-			if (container.getInput().isKeyPressed(Input.KEY_LEFT)) {
-				player.move(Direction.LEFT);
-			}
-			if (container.getInput().isKeyPressed(Input.KEY_RIGHT)) {
-				player.move(Direction.RIGHT);
-			}
-			if (container.getInput().isKeyPressed(Input.KEY_DOWN)) {
-				player.move(Direction.DOWN);
-			}
-			if (container.getInput().isKeyPressed(Input.KEY_UP)) {
-				player.move(Direction.UP);
-			}
+		transX = (player.getX() * -1) + (5*64);
+		transY = (player.getY() * -1) + (5*64);
+		if (container.getInput().isKeyPressed(Input.KEY_LEFT)) {
+			player.move(Direction.LEFT);
+		}
+		if (container.getInput().isKeyPressed(Input.KEY_RIGHT)) {
+			player.move(Direction.RIGHT);
+		}
+		if (container.getInput().isKeyPressed(Input.KEY_DOWN)) {
+			player.move(Direction.DOWN);
+		}
+		if (container.getInput().isKeyPressed(Input.KEY_UP)) {
+			player.move(Direction.UP);
+		}
 
-			/*
-			//If statements here for testing purposes, make a method for it later.
-			//Makes it so the map doesn't go out of screen bounds (A bit buggy atm).
-			if(transY > 0 && transX + map.getScaledWidth() <= container.getWidth()) {
-				transX = -container.getWidth();
-				transY = 0;
-			} else if (transX + map.getScaledWidth() <= container.getWidth() && transY + map.getScaledHeight() <= container.getHeight()) {
-				transX = -container.getWidth();
-				transY = -container.getHeight();
-			} else if (transX > 0 && transY + map.getScaledWidth() <= container.getHeight()) {
-				transX = 0;
-				transY = -container.getHeight();
-			} else if (transY + map.getScaledHeight() <= container.getHeight()) {
-				transY = -container.getHeight();
-			} else if (transX + map.getScaledWidth() <= container.getWidth()) {
-				transX = -container.getWidth();
-			} else if(transX > 0) {
-				transX = 0;
-			} else if (transY > 0) {
-				transY = 0;
-			} else if (transX > 0 && transY > 0) {
-				transX = 0;
-				transY = 0;
-			}
-			*/
-			//System.out.println(transY);
+		/*
+		//If statements here for testing purposes, make a method for it later.
+		//Makes it so the map doesn't go out of screen bounds (A bit buggy atm).
+		if(transY > 0 && transX + map.getScaledWidth() <= container.getWidth()) {
+			transX = -container.getWidth();
+			transY = 0;
+		} else if (transX + map.getScaledWidth() <= container.getWidth() && transY + map.getScaledHeight() <= container.getHeight()) {
+			transX = -container.getWidth();
+			transY = -container.getHeight();
+		} else if (transX > 0 && transY + map.getScaledWidth() <= container.getHeight()) {
+			transX = 0;
+			transY = -container.getHeight();
+		} else if (transY + map.getScaledHeight() <= container.getHeight()) {
+			transY = -container.getHeight();
+		} else if (transX + map.getScaledWidth() <= container.getWidth()) {
+			transX = -container.getWidth();
+		} else if(transX > 0) {
+			transX = 0;
+		} else if (transY > 0) {
+			transY = 0;
+		} else if (transX > 0 && transY > 0) {
+			transX = 0;
+			transY = 0;
+		}
+		*/
+		//System.out.println(transY);
 	}
 
 	@Override
